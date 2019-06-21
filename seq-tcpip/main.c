cce9d9d37a0f5fbea16dd03b7acc2305ef6bdcef
Kirill Morozov (moki) <morozov.kirill.moki@gmail.com>
2019-06-22 00:48:10 +0300

    implement server loop
    Sequential server loop, accepts one connection
    select between 2 sockets: tcp, std input
    
    stdin:
    	reads command, allows you to stop server
    	with "quit" command
    
    listener:
    	accept connection, receive one line
    	message, and send response greeting
    	message back.

--- seq-tcpip/main.c
+++ seq-tcpip/main.c
@@ -1,18 +1,22 @@
 #define _POSIX_C_SOURCE (200809L)
-#define _XOPEN_SOURCE (1)
+#define _XOPEN_SOURCE (700)
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/time.h>
 #include <inttypes.h>
 #include <unistd.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <netdb.h>
 #include <errno.h>
 
 #define BACKLOG (20)
+#define STDIN_FD (0)
 
 static void usage(char **arguments);
+
+static char *welcome_message = "Hello Dear Peer.\n";
 
 int main(int argc, char **argv) {
 	/* check server parameters */
@@ -36,6 +40,8 @@ int main(int argc, char **argv) {
 	struct addrinfo *server;
 	struct addrinfo *cursor;
 	struct addrinfo  hints;
+	fd_set		_server_fds;
+	fd_set		server_fds;
 	int		reuse_addr;
 	int 		listener;
 	int		err;
@@ -90,7 +96,67 @@ int main(int argc, char **argv) {
 	}
 
 	freeaddrinfo(server);
+	fprintf(stdout, "server: listening for incoming connections on port %s\n", argv[1]);
 
+	FD_ZERO(&server_fds);
+	FD_ZERO(&_server_fds);
+
+	FD_SET(STDIN_FILENO, &_server_fds);
+	FD_SET(listener, &_server_fds);
+
+	/* server loop */
+	for (;;) {
+		struct sockaddr_storage peer_addr;
+		socklen_t 		peer_addr_len;
+		FILE 			*peer_stream;
+		size_t 			peer_msg_len;
+		size_t 			cmd_len;
+		int 			peer;
+
+		server_fds = _server_fds;
+		char *peer_msg = NULL;
+		char *cmd = NULL;
+
+		err = select(listener + 1, &server_fds, NULL, NULL, NULL);
+		if (err == -1) {
+			fprintf(stderr, "failed to select socket that yield\n");
+			exit(EXIT_FAILURE);
+		}
+
+		if (FD_ISSET(STDIN_FILENO, &server_fds)) {
+			(void) getline(&cmd, &cmd_len, stdin);
+			if (strcmp(cmd, "quit\n") == 0)
+				exit(EXIT_SUCCESS);
+			free(cmd);
+			continue;
+		}
+
+		peer_addr_len = sizeof(struct sockaddr_storage);
+		peer = accept(listener, (struct sockaddr *) &peer_addr, &peer_addr_len);
+		if (peer == -1)
+			fprintf(stderr, "failed to serve peer\n");
+
+		peer_stream = fdopen(peer, "r");
+
+		if (!peer_stream) {
+			fprintf(stderr, "failed to read from peer\n");
+			close(peer);
+			continue;
+		}
+
+		(void ) getline(&peer_msg, &peer_msg_len, peer_stream);
+		fprintf(stdout, "peer says:\n%s", peer_msg);
+		free(peer_msg);
+
+		err = send(peer, welcome_message, strlen(welcome_message), 0);
+		if (err == -1)
+			fprintf(stderr, "failed to send data to peer\n");
+
+		fclose(peer_stream);
+		close(peer);
+	}
+
+	close(listener);
 	return EXIT_SUCCESS;
 }
 
