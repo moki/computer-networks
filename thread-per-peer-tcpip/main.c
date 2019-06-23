b85ff6d8a92615fcc3b665f04d8ca6bbd00c064d
Kirill Morozov (moki) <morozov.kirill.moki@gmail.com>
2019-06-23 06:33:34 +0300

    add server module
    data structures:
    
    struct server_attr {
            int   transport_proto;
            int   internet_proto;
            int   backlog;
            char *port;
    };
    
    functions:
    
    initialization:
    
    int server_attr_init(server_attr_t *server_attr);
    
    setters:
    
    int server_attr_setport(server_attr_t *server_attr, char *port);
    
    int server_attr_settransportproto(server_attr_t *server_attr, int protocol);
    
    int server_attr_setinternetproto(server_attr_t *server_attr, int protocol);
    
    actions:
    
    int server_listen(server_attr_t *server_attr);

---
+++ thread-per-peer-tcpip/main.c
@@ -0,0 +1,61 @@
+#define _POSIX_C_SOURCE (200809L)
+#define _XOPEN_SOURCE (700)
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <inttypes.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <errno.h>
+
+#include "server.h"
+
+#define BACKLOG (20)
+#define STDIN_FD (0)
+
+static void usage(char **arguments);
+
+static char *welcome_message = "Hello Dear Peer.\n";
+
+int main(int argc, char **argv) {
+	/* check server parameters */
+	if (argc != 2) {
+		usage(argv);
+		exit(EXIT_FAILURE);
+	}
+
+	uintmax_t portnumber = strtoumax(argv[1], NULL, 10);
+	if (portnumber == UINTMAX_MAX && errno == ERANGE) {
+		fprintf(stderr, "could not convert port nubmer\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if (portnumber >= 65536) {
+		usage(argv);
+		exit(EXIT_FAILURE);
+	}
+
+	int listener;
+
+	server_attr_t server_config;
+	server_attr_init(&server_config);
+	server_attr_setport(&server_config, argv[1]);
+	server_attr_setinternetproto(&server_config, AF_UNSPEC);
+	server_attr_settransportproto(&server_config, SOCK_STREAM);
+
+	listener = server_listen(&server_config);
+	if (listener == -1) {
+		fprintf(stderr, "failed start-up server\n");
+		exit(EXIT_FAILURE);
+	}
+
+	close(listener);
+	return EXIT_SUCCESS;
+}
+
+static void usage(char **arguments) {
+	fprintf(stderr, "error, usage: %s port, where port is < 65536\n", arguments[0]);
+}
